c3ec8227eff9375c0b8318aef47b2173e3a6b475
maithri911 <shettymaithri745@gmail.com>
2024-09-10 15:13:22 +0530

    Matrix Addition

---
+++ lab1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class lab1{
+    public static void main(String[] args){
+        int N=Integer.parseInt(args[0]);
+        int[][] matrix1=new int[N][N];
+        int[][] matrix2=new int[N][N];
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the elements of the first matrix:");
+        for(int i=0;i<N;i++){
+           for(int j=0;j<N;j++){
+               matrix1[i][j]=scanner.nextInt();}}
+        System.out.println("Enter the elements of the second matrix:");
+         for(int i=0;i<N;i++){
+             for(int j=0;j<N;j++){
+                 matrix2[i][j]=scanner.nextInt();}}
+         int[][] sumMatrix=new int[N][N];
+         for(int i=0;i<N;i++){
+             for(int j=0;j<N;j++){
+                 sumMatrix[i][j]=matrix1[i][j]+matrix2[i][j];} }
+        System.out.println("The sum of two matrix is:");
+        for(int i=0;i<N;i++){
+           for(int j=0;j<N;j++){
+            System.out.print(sumMatrix[i][j]+ "  ");
+           }
+           System.out.println();
+    }
+}
+}
+       
+        
+
+
+
+        
